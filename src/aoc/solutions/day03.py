e01832006af838d31c46ae54b0f0c00b54439f91
Callum Leach <luxcaesium@protonmail.com>
2024-12-17 21:36:40 +0000

    completed day 3

--- src/aoc/solutions/day03.py
+++ src/aoc/solutions/day03.py
@@ -15,8 +15,29 @@ def part1(data):
     
     return ans
 
+def part2(data):
 
+    pattern = r"(mul|do|don\'t)\((\d+,\d+|)\)"
+
+    ans = 0
+    enabled = True
+
+    for match in re.findall(pattern, data):
+        op, args = match
+
+        if op != "mul":
+            enabled = op == "do"
+            continue
+        
+        a, b = args.split(",")
+        if not enabled or not ((1 <= len(a) <= 3) and (1 <= len(b) <= 3)):
+            continue
+
+        ans += int(a) * int(b)
+
+    return ans
 
 data = get_input(3)
 
 print(f"Part 1 Solution: {part1(data)}")
+print(f"Part 2 Solution: {part2(data)}")
